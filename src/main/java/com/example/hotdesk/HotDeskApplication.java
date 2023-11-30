c6207d2072fcb9089dfce73745f1fbba8f2e1b40
tornado7222 <turayevpaxlavon777@gmial.com>
2023-11-30 17:12:36 +0500

    Initial commit

---
+++ src/main/java/com/example/hotdesk/HotDeskApplication.java
@@ -0,0 +1,13 @@
+package com.example.hotdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotDeskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotDeskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hotdesk/HotDeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hotdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotDeskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
