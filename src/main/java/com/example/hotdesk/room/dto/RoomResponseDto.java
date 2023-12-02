a42fa9a17c4271cf5b6ad7c2863d654a466ec025
tornado7222 <turayevpaxlavon777@gmial.com>
2023-12-03 03:16:55 +0500

    initial commit

--- src/main/java/com/example/hotdesk/room/dto/RoomResponseDto.java
+++ src/main/java/com/example/hotdesk/room/dto/RoomResponseDto.java
@@ -1,8 +1,6 @@
 package com.example.hotdesk.room.dto;
 
 import com.example.hotdesk.room.entity.RoomType;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,10 +10,8 @@ import lombok.NoArgsConstructor;
 @Data
 public class RoomResponseDto {
     private Integer id;
-    @NotBlank
     private String number;
     private RoomType roomType;
-    @NotNull
     private Integer floorNumber;
     private Integer officeId;
 }
